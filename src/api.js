7c0abbec101ddc0e2429c7ce45e10a46e2c40a04
saiganesh95-cr7 <134715110+saiganesh95-cr7@users.noreply.github.com>
2024-02-28 21:30:29 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cf6187d9b5msh225c199de83f41ep1832fdjsnb39d7ff052c3",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "114b014c1f24ca16b23df73b294a475b"; // enter your key from openweather API
